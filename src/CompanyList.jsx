f9db10a7a4d5cb1526765b15f6121a4d11e7b83c
Jacob <jacobrashid404@gmail.com>
2024-06-05 16:22:28 -0700

    component now fetches data from real back end

--- src/CompanyList.jsx
+++ src/CompanyList.jsx
@@ -1,5 +1,7 @@
 import SearchForm from "./SearchForm.jsx";
 import CompanyCardList from "./CompanyCardList.jsx";
+import JoblyApi from "./api.js";
+import { useState, useEffect } from "react";
 
 /** List all companies
  *
@@ -9,25 +11,62 @@ import CompanyCardList from "./CompanyCardList.jsx";
  * RoutesList -> CompanyList -> {SearchForm, CompanyCardList}
  */
 
-const defaultData = {
-  companies: [
-    {
-      handle: "j_and_a",
-      name: "Jacob and Aubrey LLC",
-      description: "white hat hacking since 2024",
-      numEmployees: 3,
-      logoUrl: 'https://cdn11.bigcommerce.com/s-nf2x4/images/stencil/960w/products/581/9741/Cowboy-white-Rubber-Duck-Adline-2__09087.1653230173.jpg'
-    }
-  ]
-};
+// const defaultData = {
+//   companies: [
+//     {
+//       handle: "j_and_a",
+//       name: "Jacob and Aubrey LLC",
+//       description: "white hat hacking since 2024",
+//       numEmployees: 3,
+//       logoUrl: 'https://cdn11.bigcommerce.com/s-nf2x4/images/stencil/960w/products/581/9741/Cowboy-white-Rubber-Duck-Adline-2__09087.1653230173.jpg'
+//     }
+//   ]
+// };
 
 function CompanyList() {
   console.log("* CompanyList");
+  const [companies, setCompanies] = useState({data: null, isLoading: true});
+  const [searchParam, setSearchParam] = useState("");
+
+  useEffect(function fetchCompaniesOnMount() {
+    async function fetchCompanies() {
+      const companyResponse = await JoblyApi.request("companies");
+      setCompanies(
+        {
+          data: companyResponse.companies,
+          isLoading: false
+        }
+      );
+    }
+    fetchCompanies();
+  }, [ ]);
+
+
+  useEffect(function fetchCompaniesOnSearch() {
+    companies.isLoading = true;
+    async function fetchCompanies() {
+      const companyResponse = await JoblyApi.request("companies", {nameLike: searchParam});
+      setCompanies(
+        {
+          data: companyResponse.companies,
+          isLoading: false
+        }
+      );
+    }
+    fetchCompanies();
+  }, [searchParam]);
+
+
+  function onCompanySearch(searchParam) {
+    setSearchParam(searchParam);
+  }
+
+  if (companies.isLoading) return <p>Loading...</p>;
 
   return (
     <div>
-      <SearchForm />
-      <CompanyCardList companies={defaultData.companies} />
+      <SearchForm onSubmit={onCompanySearch}/>
+      <CompanyCardList companies={companies.data} />
     </div>
   );
 };
