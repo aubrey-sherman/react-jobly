248095fd776ad53e68beab99a8257b1e0864b7a4
Jacob <jacobrashid404@gmail.com>
2024-06-05 16:23:02 -0700

    search form now has handleChange and handleSubmit functionality

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
+
 /** Search bar for Jobly
  *
- * Props: None
- * State: None
+ * Props: onSubmit function
+ * State: formData
  *
  * {CompanyList, JobList} -> SearchForm
  */
 
-function SearchForm() {
+function SearchForm({ onSubmit }) {
+  const [formData, setFormData] = useState(null);
   console.log("* SearchForm");
 
+  function handleChange(evt) {
+    const fieldName = evt.target.name;
+    const value = evt.target.value;
+
+    setFormData(currData => {
+      currData[fieldName] = value;
+      return { ...currData };
+    });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onSubmit(formData.searchParam);
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row justify-content-center">
         <div className="col-8">
-          <input type="text" className="form-control form-control-lg" placeholder="Enter search term..." />
+          <input type="text"
+                 name="searchParam"
+                 className="form-control form-control-lg"
+                 placeholder="Enter search term..."
+                 onChange={handleChange}
+          />
         </div>
         <div className="col-auto">
           <button type="submit" className="btn btn-lg btn-primary">Search</button>
