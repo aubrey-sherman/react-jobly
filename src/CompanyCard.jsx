4b1a9f050ea220024caea6d2ac880fc24a6ae8e3
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-05 17:35:01 -0400

    Added image and updated imports

--- src/CompanyCard.jsx
+++ src/CompanyCard.jsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
+import "./CompanyCard.css";
+
 /** Company card
  *
  * Props: company object
  * State: None
  *
- * CompanyList -> CompanyCard
+ * CompanyCardList -> CompanyCard
  */
 
 const defaultData = {
+  handle: "j_and_a",
   name: "Jacob and Aubrey LLC",
   description: "white hat hacking since 2024",
   jobs: [
@@ -19,12 +23,18 @@ const defaultData = {
   ]
 };
 
-function CompanyCard({ company=defaultData }) {
+function CompanyCard({ company = defaultData }) {
+  console.log("* CompanyCard", company);
+
+
   return (
-    <div>
-      <p><b>{company.name}</b></p>
-      <p>{company.description}</p>
-    </div>
+    <NavLink to={`/companies/${company.handle}`}>
+      <div>
+        <img src="https://cdn11.bigcommerce.com/s-nf2x4/images/stencil/960w/products/581/9741/Cowboy-white-Rubber-Duck-Adline-2__09087.1653230173.jpg" />
+        <p><b>{company.name}</b></p>
+        <p>{company.description}</p>
+      </div>
+    </NavLink>
   );
 }
 
