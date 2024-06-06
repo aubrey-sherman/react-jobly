9acf7fdd8ac6759c19932ad749a732f1ccea7bd1
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-06 18:35:07 -0400

    New component to hold state on user

---
+++ src/JoblyApp.jsx
@@ -0,0 +1,21 @@
+
+import RoutesList from './RoutesList.jsx';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+ * App -> RoutesList
+*/
+
+function JoblyApp() {
+  return (
+    <div className="JoblyApp">
+      <RoutesList />
+    </div>
+  );
+};
+
+export default JoblyApp;
