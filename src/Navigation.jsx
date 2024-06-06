dfabef6cb0d2d8bad4049bb802d73765d8e93a74
Jacob <jacobrashid404@gmail.com>
2024-06-06 16:11:42 -0700

    navigation options show conditionally based on currUser state

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,13 +2,20 @@ import { NavLink } from "react-router-dom";
 
 /** Navbar for Jobly
  *
- * Props: None
+ * Props: currUser => {
+ *                      username,
+ *                      firstName,
+ *                      lastName,
+ *                      isAdmin,
+ *                      email,
+ *                      applications: [jobID, ...]
+ *                    }
  * State: None
  *
  * App -> Navigation
  */
 
-function Navigation() {
+function Navigation({ currUser }) {
   console.log("* Navigation");
 
   return (
@@ -21,20 +28,60 @@ function Navigation() {
             Home
           </NavLink>
         </li>
-        <li className="nav-item px-2">
-          <NavLink
-            to={'/companies'}
-          >
-            Companies
-          </NavLink>
-        </li>
-        <li className="nav-item px-2">
-          <NavLink
-            to={'/jobs'}
-          >
-            Jobs
-          </NavLink>
-        </li>
+        {currUser === null
+        ?
+        <div>
+          <li className="nav-item px-2">
+            <NavLink
+              to={'/login'}
+            >
+              Login
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to={'/signup'}
+            >
+              Signup
+            </NavLink>
+          </li>
+        </div>
+        :
+        <div>
+          <li className="nav-item px-2">
+            <NavLink
+              to={'/companies'}
+            >
+              Companies
+            </NavLink>
+          </li>
+
+          <li className="nav-item px-2">
+            <NavLink
+              to={'/jobs'}
+            >
+              Jobs
+            </NavLink>
+          </li>
+
+          <li className="nav-item px-2">
+            <NavLink
+              to={'/profile'}
+            >
+              Profile
+            </NavLink>
+          </li>
+
+          <li className="nav-item px-2">
+            <NavLink
+              to={'/logout'}
+            >
+              Logout {currUser.username}
+            </NavLink>
+          </li>
+        </div>
+        }
       </ul>
     </nav>
   );
