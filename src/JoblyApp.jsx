3c6a9a9ffdf5031ffaf04187cb02dad60475bdc6
Jacob <jacobrashid404@gmail.com>
2024-06-06 16:10:53 -0700

    currUser state, keeps track of logged-in user status

--- src/JoblyApp.jsx
+++ src/JoblyApp.jsx
@@ -1,19 +1,28 @@
-
+import { useState } from 'react';
 import RoutesList from './RoutesList.jsx';
 
 
 /** Component for entire page.
  *
  * Props: none
- * State: none
+ * State: currUser => {
+ *                      username,
+ *                      firstName,
+ *                      lastName,
+ *                      isAdmin,
+ *                      email,
+ *                      applications: [jobID, ...]
+ *                    }
  *
  * App -> RoutesList
 */
 
 function JoblyApp() {
+  const [currUser, setCurrUser] = useState(null);
+
   return (
     <div className="JoblyApp">
-      <RoutesList />
+      <RoutesList currUser={currUser}/>
     </div>
   );
 };
