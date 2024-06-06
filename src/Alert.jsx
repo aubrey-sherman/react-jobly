2674dbdddee3bac2117b73ced702eadef26eb9e1
Jacob <jacobrashid404@gmail.com>
2024-06-06 16:10:34 -0700

    alert component to display form alerts

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,6 +1,18 @@
-/**  */
-function Alert() {
-
+/** Shows alerts from forms
+ *
+ * Props: array of alert messages
+ * State: None
+ *
+ *
+ */
+function Alert({ messages }) {
+  return (
+    <div>
+      {messages.map(message => {
+        return <div>{message}</div>
+      })}
+    </div>
+  );
 }
 
 export default Alert;
