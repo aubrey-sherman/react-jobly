c4fc8b9e6f99b6fdec4abb058720a266fc913234
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-06 20:01:17 -0400

    handleLogin added

--- src/JoblyApp.jsx
+++ src/JoblyApp.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import RoutesList from './RoutesList.jsx';
+import JoblyApi from './api.js';
 
 
 /** Component for entire page.
  *
  * Props: none
- * State: currUser => {
+ * State:
+ *        currUser => {
  *                      username,
  *                      firstName,
  *                      lastName,
@@ -13,16 +15,35 @@ import RoutesList from './RoutesList.jsx';
  *                      email,
  *                      applications: [jobID, ...]
  *                    }
+ *        token (string)
  *
  * App -> RoutesList
 */
 
 function JoblyApp() {
   const [currUser, setCurrUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  /** Logs in a user with a valid username/password.
+   *
+   * Updates state with token.
+   * If login fails to authenticate, renders error message.
+  */
+  async function handleLogin(formData) {
+    const loginResponse = await JoblyApi.logInUser(formData);
+    if (loginResponse.token) {
+      setToken(JoblyApi.token);
+    } else if (loginResponse.error) {
+      return <Alert messages={loginResponse.error} />;
+    }
+  }
+
+  // TODO: write effect with token as dependency
+
 
   return (
     <div className="JoblyApp">
-      <RoutesList currUser={currUser}/>
+      <RoutesList currUser={currUser} />
     </div>
   );
 };
