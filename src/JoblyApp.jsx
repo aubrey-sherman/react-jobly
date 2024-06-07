187ad812b549d1257bbcecd614a0fa0eccd27a6d
Jacob <jacobrashid404@gmail.com>
2024-06-06 18:03:02 -0700

    handleLogin WIP, useEffect for currUser added

--- src/JoblyApp.jsx
+++ src/JoblyApp.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import RoutesList from './RoutesList.jsx';
 import JoblyApi from './api.js';
 
@@ -22,28 +23,54 @@ import JoblyApi from './api.js';
 
 function JoblyApp() {
   const [currUser, setCurrUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState("");
 
   /** Logs in a user with a valid username/password.
    *
    * Updates state with token.
    * If login fails to authenticate, renders error message.
   */
+
+  //TODO: probably need to try/catch the logInUser method. Figure out how to catch bad login input.
   async function handleLogin(formData) {
+    console.log("handleLogin: formData", formData);
     const loginResponse = await JoblyApi.logInUser(formData);
-    if (loginResponse.token) {
+    console.log("loginResponse", loginResponse);
+    if (loginResponse) {
+      console.log("setToken, setCurrUser");
       setToken(JoblyApi.token);
-    } else if (loginResponse.error) {
+      setCurrUser({
+        username: formData.username
+      });
+      <Navigate to={"/"} />;
+    } else if (!loginResponse) {
       return <Alert messages={loginResponse.error} />;
     }
   }
 
-  // TODO: write effect with token as dependency
+  //TODO: make handleSignup and handleLogout functions
+
+  useEffect(function fetchUserDataOnLogin() {
+    async function fetchUserData() {
+      if (currUser !== null) {
+        const userResponse = await JoblyApi.getUserDetails(currUser.username);
+        setCurrUser(currUserData => ({
+          ...currUserData,
+          firstName: userResponse.firstName,
+          lastName: userResponse.lastName,
+          email: userResponse.email,
+          isAdmin: userResponse.isAdmin,
+          applications: userResponse.applications
+        }));
+      }
+    }
+    fetchUserData();
+  }, [token]);
 
 
   return (
     <div className="JoblyApp">
-      <RoutesList currUser={currUser} />
+      <RoutesList currUser={currUser} handleLogin={handleLogin} />
     </div>
   );
 };
