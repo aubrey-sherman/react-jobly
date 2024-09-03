182a89ce287a30a8cd72a212c138329cc8650a38
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-09-03 15:08:17 -0400

    Update landing page text

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -18,11 +18,11 @@ function Homepage() {
     <div class="homepage">
       <div class="container text-center">
         <h1>Jobly</h1>
-        <h3>All the jobs in one convenient place</h3>
+        <h3>All the jobs, in one convenient place</h3>
         {user && <h2>Welcome back, {user.firstName}!</h2>}
         {!user &&
           <>
-            <Link to={'/login'} className="btn btn-primary fw-bold me-3">Login</Link>
+            <Link to={'/login'} className="btn btn-primary fw-bold me-3">Log In</Link>
             <Link to={'/signup'} className="btn btn-primary fw-bold me-3">Sign Up</Link>
           </>
         }
